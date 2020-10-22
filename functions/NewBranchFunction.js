0bb2cd8947a39181e93067651394548b6987260e
Anupam Singh <53266699+anupamsinghdoon@users.noreply.github.com>
2020-10-22 21:26:38 +0530

    Create NewBranchFunction.js

---
+++ functions/NewBranchFunction.js
@@ -0,0 +1,7 @@
+exports.handler = function(context, event, callback) {
+    const result = {
+        message : "This Function created only for Anupam Branch on Twilio"
+    }
+
+    callback(null, result);
+  };
